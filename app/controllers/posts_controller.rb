bf404888a10f4184ac0d99422393c0898eea8218
tangor <otto3.teruteru.f@gmail.com>
2023-09-19 14:22:27 +0900

    toppage

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,5 @@
+class PostsController < ApplicationController
+    def toppage
+        render :toppage
+    end    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #PostController
+  get '/', to: 'posts#toppage', as: 'toppage_posts'
 end
